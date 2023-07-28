71fb2a7b32570023fb8836d323f7788a39701d12
parksr89 <54193969+parksr89@users.noreply.github.com>
2023-07-27 21:45:25 -0700

    final finished code

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React from "react";
 import './App.css';
-import { HashRouter, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import About from './routes/About';
+import Home from "./routes/Home";
+import Navigation from "./components/Navigation";
+import Detail from "./routes/Details";
 
- function App(){
+
+
+function App(){
   return (
     <HashRouter>
-     <Route path="/about" component={About} />
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/movie-detail" element={<Detail/>} />
+      </Routes>
     </HashRouter>
   );
- }
+}
 
 export default App;
---
+++ src/routes/Details.js
@@ -0,0 +1,21 @@
+import React, { useEffect } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+
+const Detail = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!location.state) {
+      navigate('/');
+    }
+  }, [location, navigate]);
+
+  if (location.state) {
+    return <span>{location.state.title}</span>;
+  } else {
+    return null;
+  }
+}
+
+export default Detail;
